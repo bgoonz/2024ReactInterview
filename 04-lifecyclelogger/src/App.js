c3387b187c7ed809ca10d2bc028e4fc931426966
Bryan C Guner <bryan.guner@gmail.com>
2024-02-07 15:31:35 -0500

    lifecycle logger

--- 04-lifecyclelogger/src/App.js
+++ 04-lifecyclelogger/src/App.js
@@ -1,5 +1,26 @@
+import {useState,useEffect} from 'react';
+
 function App() {
-  return <div></div>;
+    const [count, setCount] = useState(0);
+    useEffect(() => {
+        console.log('component mount')
+        return ()=>{
+            console.log('unmount')
+        }
+    }, []);
+    
+    useEffect(() => {
+        if(count >0){
+        console.log('update')
+        }
+    }, [count]);
+    
+  return (
+  <div>
+    <h2>Count:{count}</h2>
+    <button onClick={()=>setCount(count +1)}>Increment</button>
+  </div>
+  );
 }
 
 export default App;

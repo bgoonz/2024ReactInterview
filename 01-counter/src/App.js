db6fec46204f53df262778009df4885ed497d7c8
Bryan C Guner <bryan.guner@gmail.com>
2024-02-07 11:28:59 -0500

    counter done

--- 01-counter/src/App.js
+++ 01-counter/src/App.js
@@ -1,5 +1,33 @@
+import { useState, useEffect } from "react";
+
 function App() {
-  return <div></div>;
+  const [count, setCount] = useState(0);
+  const [ascending, setAscending] = useState(true);
+  useEffect(() => {
+    if (ascending && count === 5) {
+      setAscending((prevDirection) => !prevDirection);
+    }
+    if (!ascending && count === 0) {
+      setAscending((prevDirection) => !prevDirection);
+    }
+  }, [count]);
+
+  function handleClick() {
+    if (ascending) {
+      setCount(count + 1);
+    } else {
+      setCount(count - 1);
+    }
+  }
+
+  return (
+    <div>
+      <button onClick={handleClick}>{ascending ? "increment" : "decrement"}</button>
+      <div>
+        <span>count:{count}</span>
+      </div>
+    </div>
+  );
 }
 
 export default App;

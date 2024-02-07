17cc5e0e8f6ad772e3586bcd018f629cc614e871
Bryan C Guner <bryan.guner@gmail.com>
2024-02-07 15:33:52 -0500

    setup for data fetching

--- 04-lifecyclelogger/src/App.js
+++ 04-lifecyclelogger/src/App.js
@@ -1,25 +1,25 @@
-import {useState,useEffect} from 'react';
+import { useState, useEffect } from "react";
 
 function App() {
-    const [count, setCount] = useState(0);
-    useEffect(() => {
-        console.log('component mount')
-        return ()=>{
-            console.log('unmount')
-        }
-    }, []);
-    
-    useEffect(() => {
-        if(count >0){
-        console.log('update')
-        }
-    }, [count]);
-    
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    console.log("component mount");
+    return () => {
+      console.log("unmount");
+    };
+  }, []);
+
+  useEffect(() => {
+    if (count > 0) {
+      console.log("update");
+    }
+  }, [count]);
+
   return (
-  <div>
-    <h2>Count:{count}</h2>
-    <button onClick={()=>setCount(count +1)}>Increment</button>
-  </div>
+    <div>
+      <h2>Count:{count}</h2>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
   );
 }
 
